9e30ead4238c4a0fd2ea7b5655ff6c576cfcd26a
Elena Lapshyna <cc020186ses@gmail.com>
2023-06-24 09:21:13 -0400

    Dummy feature modules

---
+++ src/app/create-wallet/create-wallet.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CreateWalletRoutingModule } from './create-wallet-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    CreateWalletRoutingModule
+  ]
+})
+export class CreateWalletModule { }
